91341261a220963fc40d20a495bd478cd567245d
Chul Park <Chul Park>
2017-01-15 16:46:28 +0900

    Initial commit

---
+++ ex12.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# 파일 인코딩 ANSI
+
+age = raw_input("몇살? ")
+height = raw_input("키는? ")
+weight = raw_input("나이는? ")
+
+print "나이는 %r살, 키는 %r cm, 몸무게는 %r kg." %(age, height, weight)
+
+print "태양의 각 지름은 %r" % '''32'10"'''
+print "태양의 각 지름은 %s" % '''32'10"'''
---
+++ ex2.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# comment
+
+print "한글 출력
